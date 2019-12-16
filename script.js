8ccdbf767264e217c5c21dfc982457fefcfbf525
Alex Korjeski <akorjeski@gmail.com>
2019-12-16 11:52:48 -0500

    messing with local storage variables. Everything is established but i cant get it to stay after refreshing the page

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function populateDateTime(){
     timedateHolder.text(moment().format("dddd, MMMM Do, YYYY"));
     console.log(n);
 }
-
+///These are the functions that control color!
 function NineAMcolorCoded(){
    if (n === 9){
     NineAMtimeslot.css("background-color", "green");
@@ -43,7 +43,7 @@ function TenAMcolorCoded(){
     TenAMtimeslot.css("background-color", "gray");
     else if (n < 10)
     TenAMtimeslot.css("background-color", "blue");
- }
+}
  function ElevenAMcolorCoded(){
     if (n === 11){
     ElevenAMtimeslot.css("background-color", "green");
@@ -52,7 +52,7 @@ function TenAMcolorCoded(){
     ElevenAMtimeslot.css("background-color", "gray");
     else if (n < 11)
     ElevenAMtimeslot.css("background-color", "blue");
- }
+}
  function TwelveAMcolorCoded(){
     if (n === 12){
     Noontimeslot.css("background-color", "green");
@@ -61,7 +61,7 @@ function TenAMcolorCoded(){
     Noontimeslot.css("background-color", "gray");
     else if (n < 12)
     Noontimeslot.css("background-color", "blue");
- }
+}
  function OnePMcolorCoded(){
     if (n === 13){
     Onetimeslot.css("background-color", "green");
@@ -70,7 +70,7 @@ function TenAMcolorCoded(){
     Onetimeslot.css("background-color", "gray");
     else if (n < 13)
     Onetimeslot.css("background-color", "blue");
- }
+}
  function TwoPMcolorCoded(){
     if (n === 14){
     Twotimeslot.css("background-color", "green");
@@ -79,7 +79,7 @@ function TenAMcolorCoded(){
     Twotimeslot.css("background-color", "gray");
     else if (n < 14)
     Twotimeslot.css("background-color", "blue");
- }
+}
  function ThreePMcolorCoded(){
     if (n === 15){
     Threetimeslot.css("background-color", "green");
@@ -88,7 +88,7 @@ function TenAMcolorCoded(){
     Threetimeslot.css("background-color", "gray");
     else if (n < 15)
     Threetimeslot.css("background-color", "blue");
- }
+}
  function FourPMcolorCoded(){
     if (n === 16){
     Fourtimeslot.css("background-color", "green");
@@ -97,7 +97,7 @@ function TenAMcolorCoded(){
     Fourtimeslot.css("background-color", "gray");
     else if (n < 16)
     Fourtimeslot.css("background-color", "blue");
- }
+}
  function FivePMcolorCoded(){
     if (n === 17){
     Fivetimeslot.css("background-color", "green");
@@ -106,15 +106,7 @@ function TenAMcolorCoded(){
     Fivetimeslot.css("background-color", "gray");
     else if (n < 17)
     Fivetimeslot.css("background-color", "blue");
- }
-    // else if (NineAMtimeslot.hourcounter < n){
-    //     NineAMtimeslot.css("background-color", "red");
-    // }
-    // else if (NineAMtimeslot.hourcounter > n){
-    //     NineAMtimeslot.css("background-color", "green")
-    // }
-    
-
+}
 
 ///Invoke all functions here (date thing) (color coded stuff)
 populateDateTime();
@@ -128,11 +120,52 @@ ThreePMcolorCoded();
 FourPMcolorCoded(); 
 FivePMcolorCoded();
 
+ function createEmptyTimeslots(){
+    localStorage.setItem("9amText", 'Can you overwrite me?');
+    localStorage.setItem("10amText", '');
+    localStorage.setItem("11amText", '');
+    localStorage.setItem("12pmText", '');
+    localStorage.setItem("1pmText", '');
+    localStorage.setItem("2pmText", '');
+    localStorage.setItem("3pmText", '');
+    localStorage.setItem("4pmText", '');
+    localStorage.setItem("5pmText", '');
 
+ }
+
+ function setEmptyTimeslots(){
+    var local9 = localStorage.getItem("9amText");
+    var local10 = localStorage.getItem("10amText");
+    var local11 = localStorage.getItem("11amText");
+    var local12 = localStorage.getItem("12pmText");
+    var local13 = localStorage.getItem("1pmText");
+    var local14 = localStorage.getItem("2pmText");
+    var local15 = localStorage.getItem("3pmText");
+    var local16 = localStorage.getItem("4pmText");
+    var local17 = localStorage.getItem("5pmText");
+
+    NineAMtimeslot.val(local9);
+    TenAMtimeslot.val(local10);
+    ElevenAMtimeslot.val(local11);
+    Noontimeslot.val(local12);
+    Onetimeslot.val(local13);
+    Twotimeslot.val(local14);
+    Threetimeslot.val(local15);
+    Fourtimeslot.val(local16);
+    Fivetimeslot.val(local17);
+    
+
+ }
+ //invoke the function to set the localStorage items equal to blank
+ createEmptyTimeslots();
+ //invoke the function to set the text areas equal to blank
+ setEmptyTimeslots();
 
 NineAMButton.on("click", function(){
-    var amanda = $("#9am").val();
-    console.log(amanda);
+    localStorage.setItem("9amText", $("#9am").val())
+    
+    
 
 })
-
+// on click
+//save to local storage and set value = localstorage value
